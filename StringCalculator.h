2d37c2800202667d7635c98684806cb5db7e3c29
RushiKhandetod <90644030+RushiKhandetod@users.noreply.github.com>
2024-07-15 17:26:28 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,5 +7,6 @@ public:
     int addofnum(const std::string& input);
     void verify_NegativeNum(const std::string& input);
     std::string normalizeDelimiters(const std::string& input);
+    std::string newlinecheck(const std::string& input);
 };
 #endif
