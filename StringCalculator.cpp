2b0d4067b9f374ef917e32f324477c555c212929
RushiKhandetod <90644030+RushiKhandetod@users.noreply.github.com>
2024-07-15 17:24:50 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+#include "StringCalculator.h"
 
+std::string replaceDelimiters(const std::string& input, const std::string& delimiters) {
+    std::string modifiedInput = input;
+    std::replace_if(modifiedInput.begin(), modifiedInput.end(), [&](char c) {
+        return c == '\n' || delimiters.find(c) != std::string::npos;
+    }, ',');
+    return modifiedInput;
+}
